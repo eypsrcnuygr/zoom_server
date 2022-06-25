943773d199d3e0b438e06d137014f7792033bcfe
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-06-25 19:57:59 +0300

    Add one route and responses

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ const server = http.createServer(app);
 
 app.use(cors());
 
+let connectedUsers = [];
+let rooms = [];
+
+app.get("/api/room-exists/:roomId", (req, res) => {
+  const { roomId } = req.params;
+  const room = rooms.find((room) => room.id === roomId);
+
+  if (room) {
+    if (room.connectedUsers.length > 3) {
+      return res.send({ roomExist: true, full: true });
+    } else {
+      return res.send({ roomExist: true, full: false });
+    }
+  } else {
+    return res.send({ roomExist: false });
+  }
+});
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
